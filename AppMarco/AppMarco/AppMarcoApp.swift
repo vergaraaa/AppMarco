0b6f4890642e85fa3d2e4078de7b64cf012cd3b0
vergaraaa <67242290+vergaraaa@users.noreply.github.com>
2021-09-01 10:16:27 -0700

    Add files via upload

---
+++ AppMarco/AppMarco/AppMarcoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppMarcoApp.swift
+//  AppMarco
+//
+//  Created by user191983 on 8/29/21.
+//
+
+import SwiftUI
+
+@main
+struct AppMarcoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppMarco/AppMarco/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppMarco
+//
+//  Created by user191983 on 8/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
