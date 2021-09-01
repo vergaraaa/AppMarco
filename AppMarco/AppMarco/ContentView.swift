2fa39199a1aa70dc461786c5edcc67a0471ba47a
Edgar Vergara <edgarvm13@hotmail.com>
2021-09-01 12:28:17 -0500

    Tab Item Home

--- AppMarco/AppMarco/ContentView.swift
+++ AppMarco/AppMarco/ContentView.swift
@@ -9,8 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView{
+            Text("Home")
+                .tabItem {
+                    let menuText = Text("Home", comment: "Home")
+                    Label{
+                        menuText
+                    } icon: {
+                        Image(systemName: "house")
+                    }
+                }
+        }
     }
 }
 
