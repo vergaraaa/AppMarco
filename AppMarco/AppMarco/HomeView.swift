00794515e62602a07ce850cac363b32e724b189e
Edgar Vergara <edgarvm13@hotmail.com>
2021-09-07 17:07:12 -0700

    Changes in home view.

--- AppMarco/AppMarco/HomeView.swift
+++ AppMarco/AppMarco/HomeView.swift
@@ -13,8 +13,8 @@ struct ImageOverlay: View{
             Text("RECORRIDO VIRTUAL")
                 .font(.custom("RobotoSlap-Medium", size: 24))
                 .padding(10)
-                .foregroundColor(.black)
-        }//.background(Color.black)
+                .foregroundColor(.white)
+        }.background(Color("RosaMarco"))
         
         .padding(10)
     }
@@ -31,73 +31,71 @@ struct HomeView: View {
         
         ZStack{
             Color("BgVeige")
-            VStack(spacing: 0) {
-                HStack {
+            
+            ScrollView{
+                VStack {
+                    
                     Text("Exposiciones Actuales")
                         .multilineTextAlignment(.center)
                         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 60)
-                        .font(.custom("RobotoSlab-Medium", size:30))
+                        .font(.custom("RobotoSlab-Medium", size:35))
+                        .foregroundColor(Color("RosaMarco"))
                         .padding()
-                        }
-                        .offset(y: 3)
-                        .frame(minWidth: 0, maxHeight: 7, alignment: .topLeading)
-            
-            TabView(selection:$selected){
-                //Images
-                ForEach(1...4, id: \.self){
-                    index in
-                    ZStack(alignment: Alignment(horizontal: .trailing, vertical: .bottom), content: {
-                        GeometryReader{reader in
-                            Image("p\(index)")
-                                .resizable()
-                                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                                .offset(x:-reader.frame(in:.global).minX)
-                                .frame(width: width, height: height/2)
+                    
+                    TabView(selection:$selected){
+                        //Images
+                        ForEach(0...2, id: \.self){
+                            index in
+                            ZStack(alignment: Alignment(horizontal: .trailing, vertical: .bottom), content: {
+                                GeometryReader{reader in
+                                    Image("\(String(index))")
+                                        .resizable()
+                                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                                        .offset(x:-reader.frame(in:.global).minX)
+                                        .frame(width: width, height: height/2)
+                                        .cornerRadius(1)
+                                        .overlay(ImageOverlay(),alignment: .center)
+                                }
+                                .frame(height: height/2)
+                                .padding(10)
+                                .background(Color.white)
                                 .cornerRadius(1)
-                                .overlay(ImageOverlay(),alignment: .center)
-                        }
-                        .frame(height: height/2)
-                        .padding(10)
-                        .background(Color.white)
-                        .cornerRadius(1)
-                        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 5, y: 5)
-                        .shadow(color: Color.black.opacity(0.2), radius: 5, x: -5, y: -5)
-                        
-                        
-                        //Imagen de Autor de la obra
-                        Image("Marco1")
-                            .resizable()
-                            .aspectRatio(contentMode:  .fill)
-                            .frame(width:55, height: 55)
-                            .clipShape(Circle())
-                            .shadow(color:Color.black.opacity(0.1) , radius: 5, x: 5, y: 5)
-                            .padding(5)
-                            .background((Color("RosaMarco")))
-                            .clipShape(RoundedRectangle(cornerRadius: 2, style: .continuous))
-                            .offset(x:-15, y:25)
+                                .shadow(color: Color.black.opacity(0.2), radius: 5, x: 5, y: 5)
+                                .shadow(color: Color.black.opacity(0.2), radius: 5, x: -5, y: -5)
+                                
+                                
+                                //Imagen de Autor de la obra
+                                Image("Marco1")
+                                    .resizable()
+                                    .aspectRatio(contentMode:  .fill)
+                                    .frame(width:55, height: 55)
+                                    .clipShape(Circle())
+                                    .shadow(color:Color.black.opacity(0.1) , radius: 5, x: 5, y: 5)
+                                    .padding(5)
+                                    .background((Color("RosaMarco")))
+                                    .clipShape(RoundedRectangle(cornerRadius: 2, style: .continuous))
+                                    .offset(x:-15, y:25)
+                            })
+                            .padding(.horizontal,25)
+                            
+                        }//ForEach EXPO
+                    }//TabView
+                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                    .navigationBarTitleDisplayMode(.inline)
+                    .toolbar(content: {
+                        ToolbarItem(placement: .principal, content: {
+                            Image("LogoMarco")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 20)
+                        })
                     })
-                    .padding(.horizontal,25)
-                   
-                }//ForEach EXPO
-                
-            }//TabView
-            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar(content: {
-                ToolbarItem(placement: .principal, content: {
-                    Image("LogoMarco")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 20)
-                })
-            })
-                
-         }//VSTACK
-            
-
+                    Spacer()
+                }//VSTACK
+            } // ScrollView
         }//ZSTACK
     }
-          
+    
 }
 
 //
